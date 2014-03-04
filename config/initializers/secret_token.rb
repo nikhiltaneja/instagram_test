adac85828a42e41200eeb1f91ebb28095f17b6b4
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-03-03 17:40:34 -0700

    First commit, add rspec and postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InstagramTest::Application.config.secret_key_base = '2e1fb5c2fd3467a7b448a2bd7fcf56174918de88db5f41b1b01ff454fbfb47ad0cdb7923cc338378dbb82ad958690e217431e1b599adaf8632763a5e7f99a2f2'
